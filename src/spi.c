f3c7f61e8a264d73502041fcc3b73d5c3a4e34c7
Staroselskii Georgii <gosha371@gmail.com>
2014-11-27 19:47:04 +0300

    wip: added openspi

--- src/spi.c
+++ src/spi.c
@@ -1,9 +1,54 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
 
 #include "spi.h"
+#include "rtklib.h"
+
+#define SPI_IO_DEBUG 1
+#ifdef  SPI_IO_DEBUG
+#define debug(fmt, args ...)  do {fprintf(stderr,"%s:%d: " fmt "\n", __FUNCTION__, __LINE__, ## args); } while(0)
+#else
+#define debug(fmt, args ...)
+#endif
 
 spi_t *openspi(const char *path, int mode, char *msg)
 {
+    spi_t *device;
+    int flags;
+    
+    device = malloc(sizeof(spi_t));
+
+    if (device == NULL) {
+        perror("malloc: ");
+        return NULL;
+    }
+
+    if ((mode & STR_MODE_R) &&
+         (mode & STR_MODE_W)) {
+        flags = O_RDWR;
+    } else if (mode & STR_MODE_R) {
+        flags = O_RDONLY;
+    } else if (mode & STR_MODE_W) {
+        flags = O_WRONLY;
+    }
+
+    device->fd = open(path, flags);            
+
+    if (device->fd < 0) {
+        perror("open: ");
+        goto errout_with_free;
+    }
+
+    /* hardcoded spi mode for Ublox */
+    device->mode = SPI_MODE_0;
+
+errout_with_free:
+    free(device);
     return NULL;
 }
 
--- src/spi.h
+++ src/spi.h
@@ -1,9 +1,12 @@
 #ifndef __SPI_H__
 #define __SPI_H__
 
+#include <stdint.h>
+
 struct spi_s
 {
    int fd;   
+   uint16_t mode;
 };
 
 typedef struct spi_s spi_t;
