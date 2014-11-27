1d0ef6a566543681bd010c8e3d46da12aedaf71b
Staroselskii Georgii <gosha371@gmail.com>
2014-11-27 19:14:45 +0300

    wip: added dummy IO implementations

--- src/spi.c
+++ src/spi.c
@@ -6,3 +6,22 @@ spi_t *openspi(const char *path, int mode, char *msg)
 {
     return NULL;
 }
+
+int  writespi (spi_t *spi, unsigned char *buff, int n, char *msg)
+{
+    return 0;
+}
+
+int  readspi  (spi_t *spi, unsigned char *buff, int n, char *msg)
+{
+    return 0;
+}
+
+int  statespi (spi_t *spi)
+{
+    return 0;
+}
+
+void closespi (spi_t *spi)
+{
+}
